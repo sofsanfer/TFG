e5d23ef119978df8a6ab7b51ff22dda42f8655ef
sofsanfer <sofiasntg@gmail.com>
2020-02-17 19:40:29 +0100

    Terminada teoría de conjuntos sobre Sintaxis del Glosario

--- Logica_Proposicional/Glosario.thy
+++ Logica_Proposicional/Glosario.thy
@@ -10,6 +10,100 @@ begin
 
 (* chapter \<open>Glosario de reglas\<close> *)
 
+text \<open>En este glosario se recoge la lista de los lemas usados en
+  indicando la página del \href{http://bit.ly/2OMbjMM}{libro de HOL} 
+  donde se encuentran.\<close>
+
+section \<open>Teoría de conjuntos (7)\<close>
+
+text \<open>Los siguientes resultados corresponden a la teoría de conjuntos 
+  \href{https://n9.cl/qatp}{Set.thy}.\<close>
+
+subsection \<open>Operaciones básicas (7.3)\<close>
+
+subsubsection \<open>Subconjuntos (7.3.1)\<close>
+
+text \<open>
+  \begin{itemize}
+    \item (p.165) @{thm[mode=Rule] rev_subsetD[no_vars]} 
+      \hfill (@{text rev_subsetD})
+    \item (p.166) @{thm[mode=Rule] subset_refl[no_vars]} 
+      \hfill (@{text subset_refl})
+    \item (p.166) @{thm[mode=Rule] subset_trans[no_vars]} 
+      \hfill (@{text subset_trans})
+  \end{itemize}\<close>
+
+subsubsection \<open>El conjunto vacío (7.3.3)\<close>
+
+text \<open>
+  \begin{itemize}
+    \item (p.167) @{thm[mode=Rule] empty_subsetI[no_vars]} 
+      \hfill (@{text empty_subsetI})
+  \end{itemize}\<close>
+
+subsubsection \<open>Unión binaria (7.3.8)\<close>
+
+text \<open>
+  \begin{itemize}
+    \item (p.169) @{thm[mode=Rule] Un_iff[no_vars]} 
+      \hfill (@{text Un_iff})
+    \item (p.169) @{thm[mode=Rule] UnI1[no_vars]} 
+      \hfill (@{text UnI1})
+    \item (p.170) @{thm[mode=Rule] UnI2[no_vars]} 
+      \hfill (@{text UnI2})
+\<close>
+
+subsubsection \<open>Aumentar un conjunto - insertar (7.3.10)\<close>
+
+text \<open>
+  \begin{itemize}
+    \item (p.171) @{thm[mode=Rule] set_insert[no_vars]} 
+      \hfill (@{text set_insert})
+  \end{itemize}\<close>
+
+subsubsection \<open>Conjuntos unitarios, insertar (7.3.11)\<close>
+
+text\<open>
+  \begin{itemize}
+    \item (p.172) @{thm[mode=Rule] singletonI[no_vars]} 
+      \hfill (@{text singletonI})
+    \item (p.172) @{thm[mode=Rule] singletonD[no_vars]} 
+      \hfill (@{text singletonD})
+  \end{itemize}
+\<close>
+
+subsubsection \<open>Reglas derivadas sobre subconjuntos (7.4.2)\<close>
+
+text \<open>
+  \begin{itemize}
+    \item (p.177) @{thm[mode=Rule] Un_upper1[no_vars]} 
+      \hfill (@{text Un_upper1})
+    \item (p.177) @{thm[mode=Rule] Un_upper2[no_vars]} 
+      \hfill (@{text Un_upper2})
+  \end{itemize}\<close>
+
+subsubsection \<open>Igualdades sobre la union, intersección, inclusion, 
+  etc. (7.4.3)\<close>
+
+text \<open>
+  \begin{itemize}
+    \item (p.181) @{thm[mode=Rule] Un_absorb[no_vars]} 
+      \hfill (@{text Un_absorb})
+    \item (p.181) @{thm[mode=Rule] Un_empty_right[no_vars]} 
+      \hfill (@{text Un_empty_right})
+    \item (p.182) @{thm[mode=Rule] Un_insert_left[no_vars]} 
+      \hfill (@{text Un_insert_left})
+  \end{itemize}\<close>
+
+subsubsection \<open>Monotonía de varias operaciones (7.4.4)\<close>
+
+text \<open>
+  \begin{itemize}
+    \item (p.188) @{thm[mode=Rule] Un_mono[no_vars]} 
+      \hfill (@{text Un_mono})
+  \end{itemize}
+\<close>
+
 section \<open>Teoría de conjuntos finitos\<close>
 
 text \<open>\comentario{Poner las páginas y secciones y ordenar los lemas 
@@ -56,40 +150,6 @@ Como es habitual, hemos cambiado la notación de la definición a @{term "list'"
   \begin{itemize}
     \item[] @{thm[mode=Rule] Un_insert_left[no_vars]} 
     \hfill (@{text Un_insert_left})
-  \end{itemize}\<close>
-
-section \<open>Teoría de conjuntos\<close>
-
-text \<open>Los siguientes resultados empleados en el análisis hecho sobre la lógica proposicional 
-  corresponden a la teoría de conjuntos de Isabelle: \href{https://n9.cl/qatp}{Set.thy}.
-
-  \begin{itemize}
-    \item[] @{thm[mode=Rule] set_append[no_vars]} 
-      \hfill (@{text set_append})
-    \item[] @{thm[mode=Rule] singletonI[no_vars]} 
-      \hfill (@{text singletonI})
-    \item[] @{thm[mode=Rule] insertI1[no_vars]} 
-      \hfill (@{text insertI1})
-    \item[] @{thm[mode=Rule] Un_empty_right[no_vars]} 
-      \hfill (@{text Un_empty_right})
-    \item[] @{thm[mode=Rule] subset_trans[no_vars]} 
-      \hfill (@{text subset_trans})
-    \item[] @{thm[mode=Rule] rev_subsetD[no_vars]} 
-      \hfill (@{text rev_subsetD})
-    \item[] @{thm[mode=Rule] Un_mono[no_vars]} 
-      \hfill (@{text Un_mono})
-    \item[] @{thm[mode=Rule] Un_upper1[no_vars]} 
-      \hfill (@{text Un_upper1})
-    \item[] @{thm[mode=Rule] Un_upper2[no_vars]} 
-      \hfill (@{text Un_upper2})
-    \item[] @{thm[mode=Rule] subset_refl[no_vars]} 
-      \hfill (@{text subset_refl})
-    \item[] @{thm[mode=Rule] empty_subsetI[no_vars]} 
-      \hfill (@{text empty_subsetI})
-    \item[] @{thm[mode=Rule] singletonD[no_vars]} 
-      \hfill (@{text singletonD})
-    \item[] @{thm[mode=Rule] Un_iff[no_vars]} 
-      \hfill (@{text Un_iff})
   \end{itemize}
 \<close>
 
